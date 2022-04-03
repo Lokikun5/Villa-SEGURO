3ca60092a8e758ef2a5a34584b6063f213bde5e9
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2022-04-03 10:36:38 +0200

    first commit

---
+++ public/js/app2.js
@@ -0,0 +1,20 @@
+const btnUp = document.querySelector('.btnUp');
+const i = document.querySelector('i');
+
+btnUp.addEventListener('click', () =>{
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+window.addEventListener("scroll", () =>{
+    if (window.scrollY > 500) {
+        i.style.color = "#FFFFFF";   
+    }else {
+        i.style.color = "#1E1E1E"; 
+    }
+}
+)
